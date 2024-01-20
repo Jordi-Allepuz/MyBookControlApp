611b5128b29be56efee3ab85ce443fa7394ae742
JordiAllepuz <valencianu@gmail.com>
2024-01-20 12:32:06 +0100

    Commit1 Creacion de archivos

--- app/src/main/java/com/example/mybookcontrolapp/MainActivity.kt
+++ app/src/main/java/com/example/mybookcontrolapp/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.mybookcontrolapp.ui.theme.MyBookControlAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,7 +24,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+//                    val navigationController = rememberNavController()
+//                    NavHost(
+//                        navController = navigationController,
+//                        startDestination = Routes..route
+//                    ) {
+//
+//                        }
                 }
             }
         }
---
+++ app/src/main/java/com/example/mybookcontrolapp/Routes.kt
@@ -0,0 +1,5 @@
+package com.example.mybookcontrolapp
+
+sealed class Routes(val route:String){
+
+}
