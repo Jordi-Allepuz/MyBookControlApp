c553f444e8c1c6a5c75b2941fb508a3fd132ce23
JordiAllepuz <valencianu@gmail.com>
2024-01-20 12:21:58 +0100

    Commit1 Inyeccion Dependecias automaticas, MainActivity y App. Añadimos en manifest permisos internet y el name de la app

--- app/src/main/java/com/example/mybookcontrolapp/MainActivity.kt
+++ app/src/main/java/com/example/mybookcontrolapp/MainActivity.kt
@@ -8,7 +8,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.mybookcontrolapp.ui.theme.MyBookControlAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/mybookcontrolapp/MyBookControlerApp.kt
@@ -0,0 +1,8 @@
+package com.example.mybookcontrolapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyBookControlerApp: Application() {
+}
